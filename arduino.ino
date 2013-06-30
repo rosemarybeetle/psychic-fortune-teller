10c1bf272f204f851403ad60c302fc538d0720ee
Rosemary Beetle <rosemarybeetle@gmail.com>
2013-06-30 10:37:55 +0100

    Add GNU

--- arduino.ino
+++ arduino.ino
@@ -3,8 +3,26 @@
   Needed to control physical interaction...
   and relay physical-detection data back to Psychic Brain Processing sketch!
   
-  @rosemarybeetle 2013
+  @rosemarybeetle 
+  30 June 2013
   http://makingweirdstuff.blogspot.com
+*/
+
+/* -----------------------
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    ----------------------
+*/
  */
  
 
